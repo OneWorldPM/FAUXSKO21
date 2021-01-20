4240bf09b3906266c826e01a9ff65ad77310ee4e
athulak <athullive@gmail.com>
2021-01-20 23:14:03 +0100

    Login submit bug fix

--- application/views/login-new.php
+++ application/views/login-new.php
@@ -90,7 +90,7 @@
                 </div>
 
                 <div class="container-login100-form-btn">
-                    <button class="login100-form-btn">
+                    <button type="button" class="login100-form-btn">
                         Login
                     </button>
                 </div>
